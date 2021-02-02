220c61ed3cbecf4ae296556bc6dcb6ba1d1768f2
willkc15 <kelton.c.williams@gmail.com>
2021-02-02 15:23:54 -0500

    remove env file

--- 01_Core/week04/ecommerce/client/contact/script.js
+++ 01_Core/week04/ecommerce/client/contact/script.js
@@ -1,7 +1,7 @@
 import {queryStrings, getQueryParamById, toggleMobileNav} from '../utils.js'
 
 const formSuccess = () => {
-    let html = '<p class="contact-form--success">Form was submitted</p>'
+    let html = '<p class="contact-form--success"><i class="far fa-check-square fa-lg" style="margin-right: 12px"></i>Form was successfully submitted</p>'
     document.querySelector(queryStrings.contactFormIntro).style.display = 'none'
     document.querySelector(queryStrings.contactFormIntro).insertAdjacentHTML('afterend', html)
 }
